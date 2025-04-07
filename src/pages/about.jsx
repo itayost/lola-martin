286f50c1e3b55e582bf9f9d11b383948fb0264a2
itayost <itayost1@gmail.com>
2025-04-07 06:26:35 +0300

    Update about.jsx

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,48 +1,43 @@
 // src/pages/about.jsx
 import React from 'react';
+import Head from 'next/head';
 import AboutHero from '../components/about/AboutHero';
 import StorySection from '../components/about/StorySection';
 import GallerySection from '../components/about/GallerySection';
 import CallToAction from '../components/about/CallToAction';
-import Head from 'next/head';
-import { generateMetadata, PAGE_METADATA } from '../utils/metadataUtils';
-import StorySection from '../src/pages/about';
+import { PAGE_METADATA } from '../utils/metadataUtils';
 
 const AboutPage = () => {
-  const pageMetadata = generateMetadata(PAGE_METADATA.about);
-
   return (
     <>
       <Head>
-        <title>{PAGE_METADATA.title}</title>
-        <meta name="description" content={PAGE_METADATA.description} />
-        <meta name="keywords" content={PAGE_METADATA.keywords} />
-        <meta name="robots" content={PAGE_METADATA.robots} />
-
+        <title>{PAGE_METADATA.about.title}</title>
+        <meta name="description" content={PAGE_METADATA.about.description} />
+        <meta name="keywords" content={PAGE_METADATA.about.keywords.join(', ')} />
+        
         {/* Open Graph */}
-        <meta property="og:title" content={PAGE_METADATA.openGraph.title} />
-        <meta property="og:description" content={PAGE_METADATA.openGraph.description} />
-        <meta property="og:url" content={PAGE_METADATA.openGraph.url} />
-        <meta property="og:type" content={PAGE_METADATA.openGraph.type} />
-        <meta property="og:site_name" content={PAGE_METADATA.openGraph.siteName} />
-        <meta property="og:image" content={PAGE_METADATA.openGraph.images[0].url} />
-        <meta property="og:image:width" content={PAGE_METADATA.openGraph.images[0].width} />
-        <meta property="og:image:height" content={PAGE_METADATA.openGraph.images[0].height} />
-        <meta property="og:image:alt" content={PAGE_METADATA.openGraph.images[0].alt} />
-
+        <meta property="og:title" content={PAGE_METADATA.about.title} />
+        <meta property="og:description" content={PAGE_METADATA.about.description} />
+        <meta property="og:url" content="https://www.lolamartin.co.il/about" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="לולה מרטין" />
+        <meta property="og:image" content={`https://www.lolamartin.co.il${PAGE_METADATA.about.image}`} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        
         {/* Twitter */}
-        <meta name="twitter:card" content={PAGE_METADATA.twitter.card} />
-        <meta name="twitter:title" content={PAGE_METADATA.twitter.title} />
-        <meta name="twitter:description" content={PAGE_METADATA.twitter.description} />
-        <meta name="twitter:image" content={PAGE_METADATA.twitter.images[0]} />
-        <meta name="twitter:site" content={PAGE_METADATA.twitter.site} />
-
-        {/* Additional meta tags */}
-      <head /> 
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={PAGE_METADATA.about.title} />
+        <meta name="twitter:description" content={PAGE_METADATA.about.description} />
+        <meta name="twitter:image" content={`https://www.lolamartin.co.il${PAGE_METADATA.about.image}`} />
+      </Head>
+      
       <AboutHero />
-      <StorySection/>
-      <GallerySection/>
+      <StorySection />
+      <GallerySection />
+      <CallToAction />
     </>
+  );
 };
 
 export default AboutPage;

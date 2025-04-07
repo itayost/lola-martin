1ca1369cc9b86fa6e2cbf1eb305dc289c0a4d4e2
itayost <itayost1@gmail.com>
2025-04-07 10:57:16 +0300

    x

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,22 +3,25 @@ import WelcomeSection from '../components/sections/WelcomeSection';
 import Moments from '../components/sections/Moments';
 import Location from '../components/sections/Location';
 import MenuHighlights from '../components/sections/MenuHighlights';
+import Head from 'next/head';
 
 export default function HomePage() {
   return (
     <>
-    <head>
-      <title>Lola Martin</title>
+      <Head>
+        <title>Lola Martin</title>
         <meta property="og:title" content="Lola Martin" />
         <meta property="og:type" content="SeaFood Bar | Restaurant" />
         <meta property="og:url" content="https://lola-martin.vercel.app/" />
         <meta property="og:image" content="/images/og-home.jpg" />
-    </head>
-    <main className="bg-card text-text dark:bg-background dark:text-white antialiased">
-      <Hero />
-      <WelcomeSection />
-      <MenuHighlights/>
-      <Moments />
-      <Location />
-    </main>
-    <>)};
+      </Head>
+      <main className="bg-card text-text dark:bg-background dark:text-white antialiased">
+        <Hero />
+        <WelcomeSection />
+        <MenuHighlights/>
+        <Moments />
+        <Location />
+      </main>
+    </>
+  );
+}

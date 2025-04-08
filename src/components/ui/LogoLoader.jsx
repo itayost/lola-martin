5b01152a664e7dc5ead30af53f19d55c57894561
itayost <itayost1@gmail.com>
2025-04-09 00:57:58 +0300

    Update LogoLoader.jsx

--- src/components/ui/LogoLoader.jsx
+++ src/components/ui/LogoLoader.jsx
@@ -10,7 +10,7 @@ import { motion } from 'framer-motion';
  * @param {string} props.bgColor - צבע הרקע (ברירת מחדל: "bg-background")
  */
 const LogoLoader = ({
-  logoSrc = "./public/images/logos/logo logo.png",
+  logoSrc = "/images/logos/lola logo.png",
   color = "border-accent",
   size = 80,
   bgColor = "bg-background"

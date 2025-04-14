c76073258d09005f30119ea01ce2729dd2e0c3c1
itayost <itayost1@gmail.com>
2025-04-14 23:44:03 +0300

    Update about.jsx

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,24 +1,19 @@
 // src/pages/about.jsx
 import React from 'react';
-import Head from 'next/head';
+import PageMeta from '../components/meta/PageMeta';
 import AboutHero from '../components/about/AboutHero';
 import WelcomeSection from '../components/about/WelcomeSection';
 import GallerySection from '../components/about/GallerySection';
-import FloatingDecorations from '../components/ui/FloatingDecorations';
+
 
 const AboutPage = () => {
   return (
     <>
-      <Head>
-        <title>אודות - לולה מרטין</title>
-        <meta name="description" content="הכירו את הסיפור של מסעדת לולה מרטין" />
-      </Head>
+      <PageMeta pageName="about" />
       
-      <div className="bg-background text-text">
+      <main className="bg-background text-text">
         <AboutHero />
         <WelcomeSection />
-        
-        {/* Enhanced gallery section with floating decorations */}
         <div className="relative overflow-hidden">
           <GallerySection />
           <FloatingDecorations 
@@ -28,7 +23,7 @@ const AboutPage = () => {
             maxSize="16px" 
           />
         </div>
-      </div>
+      </main>
     </>
   );
 };

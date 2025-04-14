87691c2e396d7e7ecdbd2ee7257e1a4d19224474
itayost <itayost1@gmail.com>
2025-04-14 22:40:55 +0300

    changing directiroies

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,11 +2,9 @@
 import React from 'react';
 import PageMeta from '../components/meta/PageMeta';
 import AboutHero from '../components/about/AboutHero';
-import WelcomeSection from '../components/sections/WelcomeSection';
+import WelcomeSection from '../components/about/WelcomeSection';
 import GallerySection from '../components/about/GallerySection';
 
-// Remove any reference to getPageMetadata
-// The PageMeta component will handle getting metadata from siteMetadata.js
 
 const AboutPage = () => {
   return (
